919d11bc3f2a16e4fdd82e1fc6814724f570a640
Gustavo Ribeiro <gurst6@gmail.com>
2020-06-13 20:16:30 -0300

    Updated sobre o projeto: Model e Banco de Dados(MYSQL)

--- Gantt_BubbleSort/Gantt_BubbleSort/src/main/java/com/spring/gantt/GanttBubbleSort/IndexController.java
+++ Gantt_BubbleSort/Gantt_BubbleSort/src/main/java/com/spring/gantt/GanttBubbleSort/IndexController.java
@@ -20,7 +20,24 @@ public class IndexController {
 	}
 	
 	@RequestMapping("/ganttTasks")
-	public String ganttTasks() {
+	public String ganttTasks(@RequestParam(name="name", required=false, defaultValue="World") 
+	String name, Model model) {
+		
+		Projects proj1 = new Projects(10, "Projeto Webbot", "12-06-2020", 0, "project");
+		Projects proj2 = new Projects(20, "Projeto Necto - Gantt", "14-06-2020", 0, "project");
+		
+		Tasks task1 = new Tasks(1, "Criar documentação de requisitos!", "12-06-2020", "14-06-2020", 0, 10);
+		Tasks task2 = new Tasks(2, "Primeira Implementação!", "15-06-2020", "17-06-2020", 0, 10);
+		Tasks task3 = new Tasks(3, "Criar documentação de requisitos!", "14-06-2020", "17-06-2020", 0, 20);
+		Tasks task4 = new Tasks(4, "Primeira Implementação!", "20-06-2020", "25-06-2020", 0, 20);
+		
+		model.addAttribute("proj1", proj1); // Atributos da  view
+		model.addAttribute("proj2", proj2); // Atributos da  view
+		model.addAttribute("task1", task1); // Atributos da  view
+		model.addAttribute("task2", task2); // Atributos da  view
+		model.addAttribute("task3", task3); // Atributos da  view
+		model.addAttribute("task4", task4); // Atributos da  view
+		
 		return "ganttTasks";
 	}
 	
